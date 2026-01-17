6e29368572dd3e935d6e0f633e1ba9969eadbee2
Eliseo Duque <duque.eliseo.7@gmail.com>
2026-01-17 17:43:06 -0600

    createt example CICD scene

---
+++ Assets/Scripts/TestBuild.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System;
+
+namespace SpatialSys.UnitySDK.Editor
+{
+    public class TestBuild : MonoBehaviour
+    {
+
+
+        // Start is called before the first frame update
+        public static void testFunction()
+        {
+            Debug.Log("Test Functio nCALLED");
+            Console.WriteLine("TEST FUNCTION CALLED");
+        }
+    }
+}
